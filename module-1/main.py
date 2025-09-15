63db25ff1a89cea4c5b7ec42fd3d7e1aa37fb773
MShalaby-BVU <mshalaby@my365.bellevue.edu>
2025-09-14 20:20:35 -0400

    Fixed spacing main.py

--- module-1/main.py
+++ module-1/main.py
@@ -8,11 +8,13 @@ This code displays the "bottle of beer" song, with the number of bottles being a
 
 def bottle_word(bottle_count):
     return "bottle" if bottle_count == 1 else "bottles"
+    
 def beer(initial_count):
     for i in range(initial_count, 0, -1):
         print(f"{i} {bottle_word(i)} of beer on the wall, {i} {bottle_word(i)} of beer.")
         print(f"Take one down, pass it around, {i - 1} {bottle_word(i-1)} of beer on the wall.")
     print("Time to buy more beer.")
+    
 def main():
     try:
         while True:
