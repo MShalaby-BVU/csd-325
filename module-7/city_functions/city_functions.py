5a9b5b6c7933983f8d4ccee9881e7065edda10fe
MShalaby-BVU <mshalaby@my365.bellevue.edu>
2025-10-26 21:43:54 -0400

    Pushing Module 7 Assignents 10/26/25

---
+++ module-7/city_functions/city_functions.py
@@ -0,0 +1,16 @@
+def citystring(city_name , country_name , population=None , language=None):
+    syntaxReturn = f"{city_name}, {country_name}"
+    if population is not None:
+        syntaxReturn += f" - Population: {population}"
+    if language is not None:
+        syntaxReturn += f", {language}"
+    return syntaxReturn
+
+
+def main():
+    print(citystring("Boston" , "United States"))
+    print(citystring("London" , "England" , 500000))
+    print(citystring("Tokyo" , "Japan" , 600000 , "Japanese"))
+
+
+main()
---
+++ module-7/city_functions/test_cities.py
@@ -0,0 +1,12 @@
+from city_functions import citystring
+
+
+def test_city_country():
+    print(citystring("Madrid" , "Spain"))
+
+
+def main():
+    test_city_country()
+
+
+main()
